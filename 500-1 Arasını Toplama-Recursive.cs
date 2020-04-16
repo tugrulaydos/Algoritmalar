f27823013d177c753b5807593216a3c26fdcf384
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-16 17:56:24 +0300

    Add files via upload

---
+++ 500-1 Arasını Toplama-Recursive.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        //500 ve 1 Arasındaki Sayıları Toplayan Program
+        static int toplam=0; // Global
+        public static void topla(int i)
+        {
+            if (i == 0)
+            {
+                return; //Metottan Çık
+            }
+
+            toplam = i + toplam; 
+            topla(i - 1);  // Burada Özyineleme Yapıyor 
+
+        }
+        static void Main(string[] args)
+        {
+            topla(500);  // 500'den Geriye Doğru Toplama İşlemi
+            Console.WriteLine(toplam);
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
