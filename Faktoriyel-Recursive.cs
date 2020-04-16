5c80a1ae45c0c692ed5d2df51c5daa1a552e55a1
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-16 19:09:45 +0300

    Add files via upload

---
+++ Faktoriyel-Recursive.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        //Recursive Faktoriyel Hesabı Yapan Kod
+        
+        public static int f(int a)
+        {             
+            if (a == 1)
+            {
+                return a;
+             }
+            
+            return a * f(a - 1); 
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Deger:");
+            int x = Int16.Parse(Console.ReadLine());
+            Console.WriteLine(f(x));
+            Console.ReadLine();            
+        }
+        
+    }
+}
