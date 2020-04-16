b8262d7e1e786e897d9704be13643747c17a9604
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-16 22:26:48 +0300

    Add files via upload

---
+++ e^x Hesaplama .cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {           
+                
+        static void Main(string[] args)
+        {
+
+            //e^x = 1 + x + x^2/2! + x^3/3! + x^4/4!..... 
+
+            Console.Write("X Degerinini Giriniz:");
+            int x = Int16.Parse(Console.ReadLine());
+            double e = 1;
+            int ex = x;
+            int f = 1;
+            for (int i = 2; i < 8; i++)
+            {
+                e += (double)ex / f;
+                ex = x * ex;
+                f = f * i;
+            }
+            Console.WriteLine(e);
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
