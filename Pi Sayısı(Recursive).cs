811a2146a8ce3772f29411ba31c1427f536f9550
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-17 14:00:07 +0300

    Add files via upload

---
+++ Pi Sayısı(Recursive).cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static int kontrol = -1;
+        public static double pi(double p , int i)
+        {
+            
+            if (i > 2000)
+            {
+                return p;
+            }
+            p += (double)1 / i * kontrol;
+
+            kontrol = kontrol * -1;
+
+            return pi(p, i + 2);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write(4 * pi(1,3));
+            Console.ReadLine();
+
+            
+        }
+    }
+}
