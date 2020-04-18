73872d9a94af91267198ea7206922d0a9ff02759
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-18 17:10:17 +0300

    Add files via upload

---
+++ Bir Sayının Rakamları Toplamı.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bir_Sayının_Rakamları_Toplamı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Herhangi Bir Sayının Rakamları Toplamını Bulan C# Programı
+            int a = 9831498;
+            int toplam = 0;
+            while (a > 0)
+            {
+                toplam = toplam + a - a / 10 * 10; //toplam + a % 10;
+                a = a / 10;
+            }
+            Console.Write(toplam);
+            Console.ReadLine();
+           
+        }
+    }
+}
