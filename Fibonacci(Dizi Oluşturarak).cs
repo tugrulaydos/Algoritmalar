483fd54c244b207372a5df04f7f23b362c0120ea
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-18 15:53:32 +0300

    Add files via upload

---
+++ Fibonacci(Dizi Oluşturarak).cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Fibonacci2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Terim Sayısını Giriniz:");
+            int terim = Int16.Parse(Console.ReadLine());
+            int[] fibo = new int[terim];
+            fibo[0] = 0;
+            fibo[1] = 1;
+            for(int i = 2; i < fibo.Length; i++)
+            {
+                fibo[i] = fibo[i - 1] + fibo[i - 2];            
+            }
+            for(int j = 0; j < fibo.Length; j++)
+            {
+                Console.Write(fibo[j] + "  ");
+
+            }
+            Console.ReadLine();
+
+            
+        }
+    }
+}
