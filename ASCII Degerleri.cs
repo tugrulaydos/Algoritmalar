7bb58270950799271f60003e334d8920d22f1b73
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-18 23:44:56 +0300

    Add files via upload

---
+++ ASCII Degerleri.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A'dan Z'ye Kadar Harfleri Ve ASCII 
+            //Degerlerinin Ekrana Yazdıran Program
+
+            char karakter;
+            int asciivalue;
+            for(karakter = 'A'; karakter <= 'Z'; karakter++)
+            {
+                asciivalue = (int)karakter;
+                Console.WriteLine("{0}:{1}", karakter,asciivalue);
+                
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
