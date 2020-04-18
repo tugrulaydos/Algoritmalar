f2dbe16032d629c38ec74c5070872c813e4739b8
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-18 23:04:18 +0300

    Add files via upload

---
+++ Bir Sayının Rakamları Toplamı(Recursive).cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        public static void Hesapla(int sayi,ref int toplam)
+        {
+            
+            if (sayi == 0)
+            {
+                return; //Metottan çıkar
+            }
+
+            toplam = toplam + sayi % 10;
+            Hesapla(sayi / 10,ref toplam);
+             
+        }
+
+        static void Main(string[] args)
+        {
+            int q = 0;
+            Console.Write("Sayı Giriniz:");
+            int sayi = Int16.Parse(Console.ReadLine());
+            Hesapla(sayi,ref q);
+            Console.Write("Rakamları Toplamı:{0}", q);
+            Console.ReadLine();
+
+
+            
+        }
+    }
+}
