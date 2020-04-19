35beac88cbb700d8787b10ce23047c8624500eef
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-19 20:02:40 +0300

    Add files via upload

---
+++ Bitwise Sayının 3.Bitindeki Değeri Yazdırmak.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //A Sayısının 3.Bitindeki Değeri Ekrana Yazırma
+
+            uint a = 0xffffffef;
+
+            uint mask = 4;
+
+            a = a & mask;
+
+            if (a > 0) { Console.WriteLine("1"); }
+
+            else { Console.WriteLine("0"); }
+
+            Console.ReadLine();
+
+        }
+    }
+}
