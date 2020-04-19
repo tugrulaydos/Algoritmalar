48e5e6459e786d4e923a2b2770f043db31b88ee1
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-19 16:35:21 +0300

    Add files via upload

---
+++ En Çok Kullanılan Harf.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String Bir İfadede En Çok Kullanılan Harfin Kaç Defa Kullanıldığını Bulan Program
+
+            string s = "faaffggafcewpffffdsa";
+            int[] dizi = new int[30];
+            for(int i = 0; i < s.Length; i++)
+            {
+                dizi[s[i] - (byte)'a']++;
+            }
+            int eb = 0;
+            for(int j = 0; j < dizi.Length; j++)
+            {
+                if (eb < dizi[j])
+                {
+                    eb = dizi[j];
+                }
+            }
+            Console.WriteLine("En Çok Kullanılan Harf {0} Defa Kullanılmıştır", eb);
+            Console.ReadLine();
+
+        }
+    }
+}
