127b5510e044114ff9d637e58f4da76a7fb3de36
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-19 16:49:21 +0300

    Add files via upload

---
+++ String'i Int'e Çevirme.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String Bir İfadeyi İnt'e Çevirme
+            int a = 0;
+            int carpim = 1;
+            string s = "123456";
+            for(int i = s.Length - 1; i >= 0; i--)
+            {
+                a = a + ((byte)s[i] - (byte)'0') * carpim;
+                carpim = carpim * 10;
+
+            }
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+        }
+    }
+}
