1787cd66ea64698413fd4f43f609d8d3118e26b7
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-19 19:44:55 +0300

    Add files via upload

---
+++ Bitwise Sayının 1.Bitini Değiştirme.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A Sayısının 1.Bitini Diğer Bitlere Zarar Vermeden Değiştiriniz.
+
+            Console.Write("A Sayısını Giriniz:");
+
+            uint a = UInt16.Parse(Console.ReadLine());           
+
+            uint mask1 = 0xfffffffe;
+            
+
+            if (a % 2 == 0)
+            {               
+                a = a | ~mask1; 
+            }
+            else
+            {
+                a = a & mask1;
+            }
+           
+            Console.WriteLine(a);
+
+            Console.ReadLine();
+
+                      
+
+
+
+
+            
+        }
+    }
+}
