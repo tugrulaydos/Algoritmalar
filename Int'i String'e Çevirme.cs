2f787166edd5ac6f6d2f9c45018fdd129c2ed33c
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-19 17:23:16 +0300

    Add files via upload

---
+++ Int'i String'e Çevirme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int İfadeyi String'e Çevirme
+
+            int a = 9346;
+            string s = "";
+            while(a > 0)
+            {
+                s = (char)(a%10 + (byte)'0') + s;
+                a = a / 10;               
+            }
+            Console.Write(s);
+            Console.ReadLine();
+
+        }
+    }
+}
