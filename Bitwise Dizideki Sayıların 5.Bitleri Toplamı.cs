7bd1ce3a88897dfefc3b6b7d902bb96e1da1c2c2
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-19 20:33:34 +0300

    Add files via upload

---
+++ Bitwise Dizideki Sayıların 5.Bitleri Toplamı.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //100 Elemanlı Bir Dizideki Sayıların 5.Bitlerindeki Değerleri Toplayıp Ekrana Yazdırmak
+
+            uint[] x = new uint[100];
+          
+            uint mask = 0x10;
+
+            uint toplam = 0;
+            
+            for(int i = 0; i < 100; i++)
+            {
+                if ((x[i] & mask) != 0)
+                {
+                    toplam++;
+                }
+                
+            }
+            Console.WriteLine(toplam);
+            Console.ReadLine();
+           
+        }
+    }
+}
