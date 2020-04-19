c5824a7760f0aaa9a2c92ea3c3549f26de3872b8
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-19 20:49:31 +0300

    Add files via upload

---
+++ Bitwise 2'lik Tabanda Kaç Adet 1 Vardır.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bir Sayının 2'lik Sayı Sistemindeki Karşılığında Kaç Adet 1 Vardır
+
+            uint a = 0xabcdef12;
+            uint mask = 1;
+            int toplam = 0;
+            for(int i = 0; i < 32; i++)
+            {
+                if ((a & mask) == 1)
+                {
+                    toplam++;
+                }
+                a = a >> 1; //mask = mask<<1;
+            }
+            Console.Write(toplam);
+            Console.ReadLine();
+
+        }
+    }
+}
