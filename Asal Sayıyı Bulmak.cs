e8398a00699d543d5e0c275b6aae6220bf459ec3
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-20 11:56:55 +0300

    Add files via upload

---
+++ Asal Sayıyı Bulmak.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir Sayının Asal Olup Olmadığını Bulan Program
+            Console.Write("Bir Sayı Giriniz:");
+            int sayi = Int16.Parse(Console.ReadLine());
+            bool asalmi=true;
+            for(int i = 2; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    asalmi = false;
+                    break;
+                }               
+
+            }
+            if (asalmi)
+            {
+                Console.Write("Asaldır");
+            }
+            else
+            {
+                Console.Write("Asal Değildir");
+            }
+        }
+    }
+}
