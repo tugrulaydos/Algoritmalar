2f8dbfaf7ba0c2a56609d7afd19573605ae0be48
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-20 12:06:05 +0300

    Add files via upload

---
+++ Asal Çarpanları Bulmak.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        public static bool Asal(int sayi)
+        {
+            bool asalmi = true;
+            for(int i = 2; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    asalmi = false;
+                    break;
+                }
+
+            }
+            return asalmi;
+        }
+        static void Main(string[] args)
+        {
+            //Bir Sayının Asal Çarpanlarını Bulmak
+            Console.Write("Bir Sayı Giriniz:");
+            int sayi = Int32.Parse(Console.ReadLine());
+            for(int i = 2; i < sayi; i++)
+            {
+                if (Asal(i))
+                {
+                    if (sayi % i == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
+              
+            }
+            Console.ReadLine();
+
+
+            
+
+        }
+    }
+}
