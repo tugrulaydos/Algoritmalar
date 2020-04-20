1069f9675443d0951f39ea088e0c292e1722dd14
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-20 18:29:19 +0300

    Add files via upload

---
+++ Bitwise Dizideki Çift Sayıları Bulmak.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //100 Elemanlı Dizi'nin Elemanlarından Kaç Tane Çift Sayı Vardır.
+
+            int[] dizi = new int[100];
+
+            int toplam = 0;
+            
+            for (int i = 0; i < 100; i++)
+            {
+                dizi[i] = i;
+                if ((dizi[i] & 1) == 0)
+                {
+                    toplam++;
+                }
+            }
+
+            Console.WriteLine("{0} Adet Çift Sayı Vardır", toplam);
+            Console.ReadLine();
+
+           
+
+
+        } 
+    }
+}
