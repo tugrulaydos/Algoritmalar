4137f2ce5e3c54e3968e85b052cf4e49d2a59a97
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-20 13:11:34 +0300

    Add files via upload

---
+++ Girilen Sayıdan Sonraki 5 Asal Sayıyı Bulma.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        public static bool Asal(int s)
+        {
+            bool asalmi = true;
+            for(int i = 2; i < s; i++)
+            {
+                if (s % i == 0)
+                {
+                    asalmi = false;
+                    break;
+                }
+            }
+            return asalmi;
+
+
+        }
+        static void Main(string[] args)
+        {
+            //Girlen Sayıdan Sonraki 5 Asal Sayıyı Bulan Program
+            Console.Write("Bir Sayı Giriniz:");
+            int sayi = Int16.Parse(Console.ReadLine());
+            int sayac = 0;
+            while (sayac < 5)
+            {
+                sayi++;
+
+                if (Asal(sayi))
+                {
+                    sayac++;
+                    Console.WriteLine(sayi);
+
+                }
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
