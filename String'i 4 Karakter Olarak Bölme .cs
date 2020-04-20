b0d56bd22cdbc3c323fef738cd13b79a4a1be48f
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-20 17:17:39 +0300

    Add files via upload

---
+++ String'i 4 Karakter Olarak Bölme .cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir String'i 4 Parçaya Bölme işlemi
+            string s = "abcdabcdabcdabcdabcdabcd";
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[i]);
+                if ((i + 1) % 4 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
