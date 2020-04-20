138245fcfcd9c87531fe01c4afbaff1053f2ebfd
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-20 17:03:54 +0300

    Add files via upload

---
+++ String İfadede Sessiz Harf Sayısını Bulmak.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Klavyeden Girilen Metindeki Sesli ve Sessiz Harf Sayasını Bulan Program
+            Console.Write("Bir Metin Giriniz:");
+            string metin = Console.ReadLine();
+            char[] sesli = new char[] { 'a', 'e', 'u', 'ü', 'i', 'ı', 'o', 'ö' };
+            metin = metin.ToLower();  //Case-Sensitivity Engellemek için Metindeki Tüm Harfleri Küçük Harf Yapar 
+            int seslisayi = 0;
+            int sessizsayi = 0;
+            bool evet = false;  //if Bloğunun Çalışıp Çalışmadığnı Kontrol Eder 
+            for (int i = 0; i < metin.Length; i++)
+            {
+                for (int j = 0; j < sesli.Length; j++)
+                {
+                    if (metin[i] == sesli[j])
+                    {
+                        evet = true;
+                        seslisayi++;
+                        break;
+
+                    }
+                }
+                if (!evet)
+                {
+                    sessizsayi++;
+                }
+                evet = false;
+            }
+            Console.WriteLine("Sesli Harf Sayısı:{0}", seslisayi);
+            Console.WriteLine("Sessiz Harf Sayısı:{0}", sessizsayi);
+            Console.ReadLine();
+        }
+    }
+}
