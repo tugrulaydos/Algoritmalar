8c0aba33284b8210406e52f1e879dadca866c99b
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-21 17:41:14 +0300

    Add files via upload

---
+++ Bitwise 1 Sayının Binary Karşılığı.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bir Sayının Binary Karşılığını Yazdırmak 
+
+            uint sayi = 0xff0000ea;
+            string s = "";
+            uint mask = 1;
+            while (sayi > 0)
+            {
+                if ((sayi & mask) == 1)
+                {
+                    s = "1" + s;
+                }
+                else
+                {
+                    s = "0" + s;
+                }
+                sayi = sayi >> 1;                
+            }
+
+            Console.Write(s);
+            Console.ReadLine();
+            
+        }
+    }
+}
