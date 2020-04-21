18e93ae2cec95a23def7eda85846fd948a661ce6
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-21 17:32:06 +0300

    Add files via upload

---
+++ Bir Kümenin Alt Kümelerini Yazırmak.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Herhangi Bir Kümenin Alt Kümelerini Ekrana Yazan Program
+
+            string s = "ABCD"; 
+            for(int i = 0; i < Math.Pow(2, s.Length);i++)
+            {
+                int mask = 1;
+                for(int j = 0 ; j < s.Length ; j++)
+                {
+                    if ((mask & i) != 0)
+                    {
+                        Console.Write(s[j]);
+                    }
+                   
+                    mask = mask << 1;
+                                        
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
