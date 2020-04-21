547f8bdc520f0752507a849792c0f5e93b5b6723
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-21 18:20:05 +0300

    Add files via upload

---
+++ Bitwise 1 Sayının Binary Karşılığı(Recursive).cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp39
+{
+    class Program
+    {
+        //Sayıyı Binary Karşılığında Recursive Metotla Yazmak 
+        public static string Binary(uint sayi)
+        {
+            
+            uint mask = 1;
+            if (sayi == 0)
+            {
+                return "";
+            }
+            if ((sayi & mask) == 1)
+            {
+                return Binary(sayi >> 1) + "1"; 
+            }
+            else
+            {
+                return Binary(sayi >> 1) + "0";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write(Binary(0xfe));
+            Console.ReadLine();
+
+        }
+    }
+}
