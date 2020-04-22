7672e772694109c497f4c3d8ac8e68404810e9e3
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-22 17:56:44 +0300

    Add files via upload

---
+++ Input Sayısına Göre Doğruluk Tablosu Oluşturmak.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp41
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input Sayısını Giriniz:");
+            int Input = Int16.Parse(Console.ReadLine());
+            string s = "";
+            for(int i = 0; i < Math.Pow(2,Input); i++)
+            {
+                int mask = 1;
+
+                for(int j = 0; j < Input ; j++)
+                {
+
+                    if ((mask & i)!=0)
+                    {
+                        s = "1 " + s;
+
+                    }
+                    else
+                    {
+                        s = "0 " + s;
+                    }
+                    mask = mask << 1;
+                }
+                Console.WriteLine(s);
+                s = "";
+            }
+            Console.ReadLine();
+          
+        }
+    }
+}
