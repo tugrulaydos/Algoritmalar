c88f738695c2fb247524622d87ed27af9d18d9b6
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-24 00:00:09 +0300

    Add files via upload

---
+++ Bitwise 2 Sayının Büyük Küçük Karşılaştırılması.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint A = 5;
+            uint B = 4;
+            for(int i = 0; i < 32; i++)
+            {
+                if ((A & 0x80000000) == (B & 0x80000000))
+                {
+                    A = A << 1;
+                    B = B << 1;
+                }
+                else
+                {
+                    if((A & 0x80000000) != 0)
+                    {
+                        Console.WriteLine("A Sayısı Büyüktür");
+                    }
+                    if((B & 0x80000000) != 0)
+                    {
+                        Console.WriteLine("B Sayısı Büyüktür");
+                    }
+                    break;
+
+                }
+            }
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
