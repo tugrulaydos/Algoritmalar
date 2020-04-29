84088ebf220bd6c577681ff7c74d2217f80f11cf
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-29 15:44:54 +0300

    Add files via upload

---
+++ Bitwise ile Bir Sayının 2.Hex Değerini 1 Artırma.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sayının Sağdan 2.Hex Değerini 1 Artırma
+
+            uint a = 0xff00ff00;
+            Console.WriteLine(a);
+            uint b = a;
+            b = b & 0x000000f0;
+            b = b + 0x10;
+            a = a & 0xffffff0f;
+            a = a | b;
+            Console.WriteLine(a);
+            Console.ReadLine();          
+
+        }
+    }
+}
