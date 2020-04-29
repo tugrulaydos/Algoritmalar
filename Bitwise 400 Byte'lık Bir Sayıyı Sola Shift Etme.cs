cf03db5827d635eea8cd33705177743af9c3c30b
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-29 18:16:57 +0300

    Add files via upload

---
+++ Bitwise 400 Byte'lık Bir Sayıyı Sola Shift Etme.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp61
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //400 Byte'lık Bir Sayıyı Sola Shift işlemi
+
+            uint[] x = new uint[100];//Dizi'nin Elemanlarını 400 Byte'lık Sayı Gibi Düşünebiliriz.
+            uint elde = 0;
+            uint oncekielde = 0;
+            for(int i = 0; i < 100; i++)
+            {
+                if ((x[i] & 0x80000000) != 0) { elde = 1; }
+
+                else { elde = 0; }
+
+                x[i] = x[i] << 1;
+
+                x[i] = x[i] + oncekielde;
+
+                oncekielde = elde;
+            }
+            
+        }
+    }
+}
