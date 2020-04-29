5952e050d25055084b030dc8785c4d3cfb354851
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-29 16:01:45 +0300

    Add files via upload

---
+++ Bitwise A Sayısının Msb 10 Bitini B Sayısına Ekleme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir Sayının Msb(Most Significant Bit-En Yüksek Seviyeli Bit)10 Bitini B Sayısına Ekleyelim.
+            uint A = 0xff00ff00;
+            uint B = 0;
+            B = A | B; // B = A Demektir
+            B = B >> 22;
+            Console.WriteLine(B);
+            Console.ReadLine();
+
+        }
+    }
+}
