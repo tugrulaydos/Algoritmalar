d9704e99f50adb717f4cf0fa45fbbb73897f7134
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-04-29 17:09:16 +0300

    Add files via upload

---
+++ Bitwise 64 Bitlik Sayıyı 2 ile Çarpmak.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp59
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 64 Bitlik Bir Sayıyı 2 ile Çarpma
+
+            uint a = 0x8000000f; //32 Bit 
+            uint b = 0x00000001; //32 Bit
+
+            //64 Bitlik Sayımız ba'dır
+
+            uint elde = 0;
+            if ((a & 0x80000000) != 0) // En Soldaki Bit 1'dir
+            {
+                elde = 1; //Eldeyi de Hesaba Katmalıyız Yoksa 1 Bit Yok Etmiş Oluruz.(Over Flow) 
+            }
+            //Sayıları 2 ile Çarpıyoruz(Sola Shift)
+            a = a << 1; 
+            b = (b << 1) + elde; 
+
+
+            //2.Çözüm
+            //b = (b << 1) + (a >> 31); 
+            //a = a << 1;
+            
+          
+           
+        }
+    }
+}
