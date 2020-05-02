e557d21ef71e1ebe72487df54d9efa1de2e649bd
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-05-02 21:53:29 +0300

    Add files via upload

---
+++ Longest Common Substring 1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //String  içerisinde Aranan İfadeyi  Bulan Program
+            //Longest Common Substring 
+            string s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string q = "QRSTUV";
+            bool bulundumu = false;
+            for(int i = 0; i <= s.Length - q.Length; i++)
+            {
+                if(q[0] == s[i])
+                {
+                    
+                    for(int j = 1; j < q.Length; j++)
+                    {
+                        if (s[i + j] != q[j]) { bulundumu = false; j = q.Length - 1; }
+
+                        else { bulundumu = true; }
+                        
+                    }
+
+                }        
+                
+
+            }
+            if (bulundumu) { Console.WriteLine("Aranan İfade Bulundu"); }
+
+            else { Console.WriteLine("Bulunamadı"); }
+
+            Console.ReadLine();
+            
+        }
+    }
+}
