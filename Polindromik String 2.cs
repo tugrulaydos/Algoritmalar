8fd582a25498fe782584d38dbd4d0b0805191af6
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-05-03 22:23:00 +0300

    Add files via upload

---
+++ Polindromik String 2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp71
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Polindromik String
+            string s = "alkazıkçakkarayakayarakkaçkızakla";
+            int left = 0;
+            int right = s.Length-1;
+            bool polindromik = false;
+            while (s[left] == s[right])
+            {
+                left++;
+                right--;                
+                if (left > right)
+                {
+                    polindromik = true;
+                    break;
+                }
+            }
+            if (polindromik)
+            {
+                Console.Write("Polindromiktir");
+            }
+            else
+            {
+                Console.Write("String Polindromik Değildir");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
