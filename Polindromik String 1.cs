73f402cd8ced789f1a6d26a559aabdc89a67272e
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-05-03 21:59:50 +0300

    Add files via upload

---
+++ Polindromik String 1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp70
+{
+    class Program
+    {			
+		static void Main(string[] args)
+        {
+            //Verilen String ifade Polindromik midir?
+            //Polindromik string --> Sağdan ve Soldan Bakıldığında Aynı Stringe Eşittir
+
+            string s = "12345678987654321";
+            string reverse = "";
+            for(int i = 0; i < s.Length; i++)
+            {
+                reverse = s[i].ToString() + reverse;
+            }
+            if (s == reverse)
+            {
+                Console.WriteLine("Polindromiktir");
+
+            }
+            else
+            {
+                Console.WriteLine("Polindromik Değildir");
+            }
+            Console.ReadLine();
+           
+
+		
+           
+        }
+    }
+}
