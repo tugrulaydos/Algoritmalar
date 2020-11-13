e70c9c0b533c3727c6da3ff8b07e9919a81d5bae
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-11-13 14:57:13 +0300

    Add files via upload

---
+++ Sort-2 Bubble_Sort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Sort_2_Bubble_Sort
+{
+    class Program
+    {
+        public static void BubbleSort(int[] array)
+        {
+            int temp = 0;
+            for(int i = 0; i < array.Length - 1; i++) 
+            {
+                for(int j = 1; j < array.Length - i; j++)
+                {
+                    if (array[j-1] > array[j])
+                    {
+                        temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+
+                    }
+
+                }
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 10, 5, 4, 2, 7, 8, 3, 1, 6, 9};
+
+            BubbleSort(array);
+
+            foreach(int i in array)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadLine();
+                        
+            
+        }
+    }
+}
