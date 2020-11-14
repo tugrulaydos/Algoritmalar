b68e9dd6e5052fdb451899fe962407e809af0795
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-11-14 15:55:04 +0300

    Add files via upload

---
+++ Linked_List2 Recursive.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Linked_List2_Recursive
+{
+    class tekli
+    {
+       public tekli next;
+       public int data;
+    } 
+    
+
+    class Program
+    {
+        static tekli head=null;
+        static tekli last=null;
+        
+
+        public static void CreateBlock(int data)
+        {
+            if (data > 10)
+            {
+                return;
+            }
+
+            head = new tekli();
+            head.data = data;
+
+            head.next = last;
+
+            last = head;
+            CreateBlock(data + 1);
+
+        }
+
+        public static void PrintSolution(tekli t)
+        {
+            if (t == null)
+            {
+                return;
+            }
+            Console.Write(t.data + "\t");
+            PrintSolution(t.next);
+
+        }
+        static void Main(string[] args)
+        {
+
+            CreateBlock(0);
+            PrintSolution(head);
+
+            Console.ReadLine();
+            
+            
+        }
+    }
+}
