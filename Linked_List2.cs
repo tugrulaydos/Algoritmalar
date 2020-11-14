626e3b93420b22b038e951b4caa318224965f2bd
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2020-11-14 15:52:01 +0300

    Add files via upload

---
+++ Linked_List2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Linked_List2
+{
+    class tekli
+    {
+        public tekli next;
+        public int data;
+
+    }
+
+    class Program
+    {                
+        static void Main(string[] args)
+        {
+            //10 adet blok oluşturma
+            tekli head = null;
+            tekli last = null;
+
+            for(int i = 0; i < 10; i++)
+            {
+                head = new tekli();
+                head.data = i;
+
+                head.next = last;
+                last = head;
+            }
+
+            tekli temp = head;
+
+            while (temp != null)
+            {
+                Console.Write(temp.data + "\t");
+                temp = temp.next; 
+            }
+
+            Console.ReadLine();
+
+
+
+
+                 
+
+                        
+
+        }
+    }
+}
