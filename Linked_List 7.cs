b830b8e4c468672dac33f2ad08eaf7661a27504b
tugrulaydos <62428662+tugrulaydos@users.noreply.github.com>
2021-01-07 10:12:57 +0300

    Add files via upload

---
+++ Linked_List 7.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp35
+{
+    class Tekli
+    {
+        public int data;
+
+        public Tekli next;
+
+    }
+    class Program
+    {
+        public static void EkranaYaz(Tekli x)
+        {
+            while (x != null)
+            {
+                Console.WriteLine(x.data);
+                x = x.next;
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            //Tekli Linked List içerisinden data degeri 23 olan bloğu siliniz.
+
+            Tekli head = null;
+            Tekli last = null;
+
+            for(int i = 0; i < 100; i++)
+            {
+                head = new Tekli();
+
+                head.data = i;
+
+                head.next = last;
+
+                last = head;
+               
+            }
+
+            
+            Tekli temp = head;          
+
+            while (temp.next.next != null)
+            {
+                if (temp.next.data == 23)
+                {
+                    temp.next = temp.next.next;
+                }
+                else
+                {
+                    temp = temp.next;
+                }
+
+            }
+
+            EkranaYaz(head);
+            Console.ReadLine();    
+            
+
+           
+            
+             
+        }
+    }
+}
